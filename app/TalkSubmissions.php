a50202623fe9a6ea364a99ac8bb9b605e69adec2
Michael Akopov <makopov@gmail.com>
2018-07-23 23:41:57 -0700

    Fixing a bug in talk submissions
        modified:   app/Http/Controllers/TalkSubmissionsController.php
        modified:   app/TalkSubmissions.php

--- app/TalkSubmissions.php
+++ app/TalkSubmissions.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class TalkSubmissions extends Model
 {
-    protected $fillable = ['name', 'email', 'title', 'abstract', 'given', 'presenter', 'date_given'];
+    protected $fillable = ['name', 'email', 'title', 'abstract', 'given', 'presenter', 'date_given', 'video_url'];
 
     public function user() {
         return $this->belongsTo('App\User');
