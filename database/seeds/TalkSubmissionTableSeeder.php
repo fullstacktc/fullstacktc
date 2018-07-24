da7c90598da1541026c0e597ef16d1e1ceac0848
Michael Akopov <makopov@gmail.com>
2018-07-23 23:54:31 -0700

    Fixing more bugs with presentation layer
        modified:   database/seeds/TalkSubmissionTableSeeder.php
        modified:   resources/views/talkgiven.blade.php

--- database/seeds/TalkSubmissionTableSeeder.php
+++ database/seeds/TalkSubmissionTableSeeder.php
@@ -11,6 +11,15 @@ class TalkSubmissionTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('talk_submissions')->insert([
+            'title' => 'Talk with no user or video',
+            'email' => str_random(6) . '@gmail.com',
+            'name' => str_random(10),
+            'abstract' => str_random(255),
+            'given' => 1,
+            'date_given' => date('Y-m-d'),
+            'video_url' => ''
+        ]);
 
         for($i = 1; $i <= 15; $i++) {
             if($i % 2 == 0) {
